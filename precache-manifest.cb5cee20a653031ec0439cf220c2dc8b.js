06d7983dbe957a3e1af5873342594418f84ef0b7
Karla Rodrigues <karlaadautor@gmail.com>
2020-10-24 12:03:51 -0400

    Updates

---
+++ precache-manifest.cb5cee20a653031ec0439cf220c2dc8b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b199647c1c29bf6d2afc6a32d78f289",
+    "url": "/NasaApp/index.html"
+  },
+  {
+    "revision": "2681d93e9434b3ded8ad",
+    "url": "/NasaApp/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "28497b894a329440284a",
+    "url": "/NasaApp/static/css/main.ffba0750.chunk.css"
+  },
+  {
+    "revision": "2681d93e9434b3ded8ad",
+    "url": "/NasaApp/static/js/2.85a31552.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/NasaApp/static/js/2.85a31552.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28497b894a329440284a",
+    "url": "/NasaApp/static/js/main.cfd73f94.chunk.js"
+  },
+  {
+    "revision": "e71d57efd63ee721350a",
+    "url": "/NasaApp/static/js/runtime-main.d6ffe0eb.js"
+  },
+  {
+    "revision": "8634ee0b51a3a4249935772422957171",
+    "url": "/NasaApp/static/media/background.8634ee0b.jpg"
+  },
+  {
+    "revision": "9ffb3c7df7ba45d3d217060f2f2f4802",
+    "url": "/NasaApp/static/media/logo.9ffb3c7d.png"
+  }
+]);
